f1a1560cb55ae856709d2f329cbb83cc19bbb459
itaiag <itai.agmon@gmail.com>
2013-10-10 13:46:21 +0300

    Initial Commit

---
+++ jbehave-tests/src/main/java/il/co/topq/auto/steps/MySteps.java
@@ -0,0 +1,32 @@
+package il.co.topq.auto.steps;
+
+import jsystem.framework.report.ListenerstManager;
+import jsystem.framework.report.Reporter;
+
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+public class MySteps {
+	protected Reporter report = ListenerstManager.getInstance();
+	
+	@Given("I am a pending step")
+	public void givenIAmAPendingStep() {
+	  report.report("In pending step");
+	}
+
+	@Given("I am still pending step")
+	public void givenIAmStillPendingStep() {
+		report.report("I am still pending step");
+	}
+
+	@When("a good soul will implement me")
+	public void whenAGoodSoulWillImplementMe() {
+		report.report("a good soul will implement me");
+	}
+
+	@Then("I shall be happy")
+	public void thenIShallBeHappy() {
+		report.report("I shall be happy");
+	}
+}
